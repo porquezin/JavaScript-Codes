e42617f29db1aa5381ad3000a38dad89d28960b8
porquezin <renatomagno75@gmail.com>
2022-03-14 16:58:30 -0300

    atividade

---
+++ exercicios/1/main.js
@@ -0,0 +1,38 @@
+var num1;
+var num2;
+
+
+function soma(){
+
+    num1 = parseInt(document.getElementById("n1").value);
+    num2 = parseInt(document.getElementById("n2").value);
+
+
+
+    document.getElementById("resp").innerText = num1 + num2;
+}
+function sub(){
+
+    num1 = parseInt(document.getElementById("n1").value);
+    num2 = parseInt(document.getElementById("n2").value);
+
+    document.getElementById("resp").innerText = num1 - num2;
+}
+function mult(){
+
+    num1 = parseInt(document.getElementById("n1").value);
+    num2 = parseInt(document.getElementById("n2").value);
+
+
+
+    document.getElementById("resp").innerText = num1 * num2;
+}
+function div(){
+
+    num1 = parseInt(document.getElementById("n1").value);
+    num2 = parseInt(document.getElementById("n2").value);
+
+
+
+    document.getElementById("resp").innerText = num1 / num2;
+}
