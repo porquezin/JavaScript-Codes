a23020632d48de9cffdb14b3590f27478224a21c
porquezin <renatomagno75@gmail.com>
2022-03-22 22:40:55 -0300

    2

--- exercicios/6/main.js
+++ exercicios/6/main.js
@@ -2,29 +2,19 @@ var nome1, nome2
 var e1, e2
 var point1 = 0 , point2 = 0
 var rodada = 0
-
 function nome() {
-
     nome1 = document.getElementById("nome1").value
     nome2 = document.getElementById("nome2").value
-    
     document.getElementById("players1").innerText = nome1 + " é par!"
     document.getElementById("players2").innerText = nome2 + " é impar!"
-    
     document.getElementById("nometudo").innerText = ""
-
     document.getElementById("t").disabled=false
 }
-
 function jogar(){
-
     e1 = parseInt(document.getElementById("esc1").value)
     e2 = parseInt(document.getElementById("esc2").value)
-    
     rodada++
-
     document.getElementById("rod").innerText = rodada
-
     if(((e1+e2)%2) == 0){
         document.getElementById("res").innerText= nome1 + " Ganhou rodada!"
         point1++
@@ -32,7 +22,6 @@ function jogar(){
         document.getElementById("res").innerText= nome2 + " Ganhou a rodada!"
         point2++
     }
-
     if(point1 == 3){
         document.getElementById("tudo").innerHTML = "<h1>" + nome1 + " ganhou tudo!</h1>"
         document.getElementById("tudo").innerHTML += "<br/><img src='/exercicios/6/index-anime.gif'>"
@@ -40,8 +29,6 @@ function jogar(){
         document.getElementById("tudo").innerHTML = "<h1>" + nome2 + " ganhou tudo!</h1>"
         document.getElementById("tudo").innerHTML += "<br><img src='/exercicios/6/index-anime.gif'>"
     }
-
     document.getElementById("pts1").innerText = " Pontos = " + point1
     document.getElementById("pts2").innerText = " Pontos = " + point2
-
 }
